c8f5cfdf26ecc9607b833d6d3c781a1fc467b579
Aditya Bhattad <adityabhattad18@gmail.com>
2023-01-11 22:45:59 +0530

    UseContext ✅

--- src/hooks-examples/UseContext.jsx
+++ src/hooks-examples/UseContext.jsx
@@ -1,13 +1,25 @@
-import { useContext } from "react";
+import { useContext, createContext } from "react";
 
+const ExampleContext = createContext(null);
 
-function UseContextExample(){
-    return (
-        <div>
-            Use Context
-        </div>
-    )
+export default function useContextExample() {
+	return (
+		<ExampleContext.Provider value="dark">
+			<ChildComponent />
+		</ExampleContext.Provider>
+	);
 }
 
+function ChildComponent() {
+	return (
+		<div>
+			<NestedChildComponent />
+		</div>
+	);
+}
 
-export default UseContextExample;
+function NestedChildComponent() {
+	const theValue = useContext(ExampleContext);
+	console.log(theValue);
+	return <div>The value was {theValue}</div>;
+}
